e3d48086d838fc068f58d3bc287d7003d6d6183e
Feyisara Agiri <mohrenny8@gmail.com>
2025-11-09 22:15:51 +0100

    Created the main file in the task 4 folder

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,35 @@
+/// <reference path="subjects/Cpp.ts" />
+/// <reference path="subjects/Java.ts" />
+/// <reference path="subjects/React.ts" />
+
+namespace Subjects {
+  // Create Teacher object with experienceTeachingC = 10
+  export const cTeacher: Teacher = {
+    firstName: 'John',
+    lastName: 'Doe',
+    experienceTeachingC: 10
+  };
+
+  // Create and export constants for each Subject
+  export const cpp = new Cpp();
+  export const java = new Java();
+  export const react = new React();
+
+  // Cpp subject
+  console.log('C++');
+  cpp.setTeacher(cTeacher);
+  console.log(cpp.getRequirements());
+  console.log(cpp.getAvailableTeacher());
+
+  // Java subject
+  console.log('Java');
+  java.setTeacher(cTeacher);
+  console.log(java.getRequirements());
+  console.log(java.getAvailableTeacher());
+
+  // React subject
+  console.log('React');
+  react.setTeacher(cTeacher);
+  console.log(react.getRequirements());
+  console.log(react.getAvailableTeacher());
+}
